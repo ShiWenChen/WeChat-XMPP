0759d8cff762ccc9578ac970c5c0fbc36400c3f9
ChenShiWen <cswzmg@live.com>
2016-04-13 16:00:31 +0800

    Initial Commit

---
+++ WeChat-XMPP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeChat-XMPP
+//
+//  Created by 小城生活 on 16/4/13.
+//  Copyright © 2016年 小城生活. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeChat-XMPP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeChat-XMPP
+//
+//  Created by 小城生活 on 16/4/13.
+//  Copyright © 2016年 小城生活. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
